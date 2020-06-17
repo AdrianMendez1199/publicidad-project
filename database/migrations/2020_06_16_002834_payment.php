9b8feb81f2aafe4a7bb8ef2993765891980bed6e
Adrian Mendez <mendezadrian149@gmail.com>
2020-06-16 20:21:54 -0400

    adding paypal config

---
+++ database/migrations/2020_06_16_002834_payment.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Payment extends Migration
+{
+   /**
+    * Run the migrations.
+    *
+    * @return void
+    */
+    public function up()
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->enum('status', ['FAILED', 'APROVED', 'PENDING']);
+ 
+            $table->unsignedBigInteger('plan_id');
+            $table->foreign('plan_id')
+             ->references('id')
+             ->on('plans');
+
+             $table->unsignedBigInteger('user_id');
+             $table->foreign('user_id')
+              ->references('id')
+              ->on('users');
+             $table->timestamps();
+        });
+    }
+ 
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payments');
+    }
+}
