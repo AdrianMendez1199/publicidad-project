bf427f7d49b47c49689c91f2d4e630f3f6c9cb90
Adrian Mendez <mendezadrian149@gmail.com>
2020-07-05 23:16:19 -0400

    working on styles

--- resources/views/user/details.blade.php
+++ resources/views/user/details.blade.php
@@ -1,102 +1,11 @@
 @extends('layouts.app')
 
-{{-- <style>
-  .header {
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: space-between;
-    background-color: #fff0f4;
-    height: auto;
-   width: 400px;
-  }
-
-.section-box {
-  color: #fe5acb;
+<style>  
+.body {
+    margin: 0 250px;
+    border-left: 1px solid #f8c1cf;
+    border-right: 1px solid #f8c1cf;
 }
-
-.valuecolumn span {
-  font-weight: bold;
-  margin: 8px;
-}
-
-.profile-header-dt {
-  display: flex;
-  text-align: center;
-  background: white;
-  height: 80px;
-  border-radius: 10%;
-}
-.valuecolumn p  {
-  color: gray;
-  margin: 5px;
-}
-.valuecolumn span {
-  color: #fe5acb;
-}
-
-
-.card-container {
-  /* max-width: 800px; */
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: flex-start;
-  cursor: zoom-in;
-}
-
-.jumbotron {
-  text-align: left;
-  width: 94% !important;
-  background: white !important;
-  padding: 20px;
-  box-shadow: 0 0 20px #e5006a;
-  margin: 8px;
-  border-radius: 15px !important;
-}
-
-
-.jumbotron h4, .jumbotron  h4 {
-    color: #e0006c;
-    padding-left: 10px;
-    margin-bottom: 10px;
-    cursor: default;
-    font-size: 1.5em;
-    border-left: 3px solid #e0006c;
-    text-transform: uppercase;
-    font-size: 1.1em;
-    font-weight: 700;
-}
-.aboutme {
-  width: 800px;
-  height: 800px;
-  background: white;
-}
-
-
-.girlsingle .profile-header .profile-header-name-info {
-    background: #fff !important;
-    padding: 10px 0 !important;
-}
-
-.r {
-    float: right !important;
-}
-
-.rad5 {
-    -webkit-border-radius: 5px !important;
-    -moz-border-radius: 5px !important;
-    border-radius: 5px !important;
-}
-
-.girlsingle .profile-header .profile-header-name-info .section-box {
-    float: right;
-    text-align: center;
-    padding: 0 20px;
-} --}}
-{{-- </style> --}}
-
-<style>
-
-  
 .bodybox {
     background-color: #fff0f4;
     padding: 20px;
@@ -182,10 +91,8 @@
     text-align: center;
 }
 
-@media (max-width: 1550px){
 .girlsingle .thumbs .profile-video-thumb-wrapper, .girlsingle .thumbs .profile-img-thumb-wrapper {
     width: 20%;
-}
 }
 
 .girlsingle .thumbs .profile-video-thumb-wrapper, .girlsingle .thumbs .profile-img-thumb-wrapper {
@@ -255,145 +162,181 @@
     font-size: 1.1em;
     font-weight: 700;
 }
+
+
+/* TO REVIEW **/
+.sidebar-right {
+    float: right;
+    /* margin-left: -250px; */
+}
+.sidebar-left, .sidebar-right {
+    width: 250px;
+    word-wrap: break-word;
+    overflow: hidden;
+}
+
+.sidebar-right .quicksearch {
+    background-color: #e0006c;
+    padding: 20px;
+    color: #fff;
+}
+
+.sidebar-right .quicksearch h4 {
+    padding-bottom: 10px;
+}
+
+.sidebar-left {
+    margin-left: -100%;
+}
+.sidebar-left, .sidebar-right {
+    width: 250px;
+    word-wrap: break-word;
+    overflow: hidden;
+}
+
+.form-styling .form-input.col100 {
+    width: 100%;
+}
+.form-styling .form-input {
+    width: 64%;
+    float: right;
+}
+.col100 {
+    width: 100% !important;
+}
+
+.sidebar-right .quicksearch .form-input select {
+    color: #286c9b;
+    cursor: pointer;
+    padding: 2px;
+}
+
+.sidebar-right .quicksearch .select2 {
+    width: 100% !important;
+}
+.form-styling .form-input select {
+    padding: 8px;
+    font-size: 1em;
+    cursor: pointer;
+}
+
+.sidebar-right .quicksearch .select2 {
+    width: 100% !important;
+}
+.select2-container {
+    box-sizing: border-box;
+    display: inline-block;
+    margin: 0;
+    position: relative;
+    vertical-align: middle;
+}
+
+.sidebar-right .quicksearch .formseparator {
+    height: 10px;
+}
+
+.form-styling .formseparator {
+    clear: both;
+    height: 30px;
+}
+
+.sidebar-right .quicksearch .submit-button {
+    border: none;
+    font-size: 1em;
+    font-weight: 700;
+}
+
+.form-styling input[type=submit] {
+    font-size: 1.2em;
+    line-height: 2em;
+    padding: 0 20px;
+    -webkit-border-radius: 25px;
+    -moz-border-radius: 25px;
+    border-radius: 25px;
+    font-weight: 600;
+}
+
+.sidebar-right .quicksearch .adv {
+    color: #fdd1ef;
+}
+.sidebar-right .quicksearch a {
+    color: #e0006c;
+    text-decoration: none;
+    cursor: pointer;
+}
+
+.blueishbutton {
+    background: #eff8ff;
+    color: #e0006c;
+    border: 1px #fe5acb solid;
+}
+
+.testing {
+  display: flex;
+    flex-wrap: wrap;
+    margin-right: -15px;
+    margin-left: -15px;
+    justify-content: center;
+    align-items: center;
+}
 </style>
 
-@section('content')
-
-{{-- <div class="container header">
-    <div class="profile-header">
-      <div class="profile-header-name l"> 
-       <h4 class="profile-title">{{ $user->name }}</h4>           
-      </div>
-    </div>
-
-    <div class="profile-header">
-        <div class="profile-header-name l"> 
-         <h4 class="profile-title">{{ $user->whatsapp }}</h4>           
-        </div>
-      </div>
-
-
-
-      <div class="profile-header-name-info rad5 r">
-        <div class="section-box">
-          <span class="valuecolumn">167</span>
-          <b>cm</b>
-        </div>
-          <div class="section-box">
-            <span class="valuecolumn">70</span><b>kg</b>
-          </div>
-          <div class="section-box">
-        <span class="valuecolumn">32</span><b>years</b>
-      </div>
-    </div>
-
-      {{-- <div class="profile-header-dt">
-              <span class="valuecolumn">
-                  <span>{{ $user->user_details->weight }}</span>
-                 <div>
-                   <p>years</p>
-                 </div>
-             </span>
-
-             <span class="valuecolumn">
-                <span>{{ $user->user_details->height }}</span>
-               <div>
-                 <p>cm</p>
-               </div>
-           </span>
-
-
-           <span class="valuecolumn">
-              <span>{{ $user->user_details->weight }}</span>
-             <div>
-               <p>cm</p>
-             </div>
-         </span>
-</div> --}}
-{{-- <div class="card-container mt-5">
-    @foreach ($user->userimages as $key => $data)
-      <div class="card ml-2 mb-2" style="width:15%;">
-        <img class="card-img" 
-         style="width:100%"
-         src="{{ asset($data->file) }}"
-         alt="Card image">                  
-      </div>
-    @endforeach
-    </div>
-    <div class="jumbotron">
-        <h4 class="display-4">Sobre mi:</h1>
-        <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-        <hr class="my-4">
-        <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-        <p class="lead">
-          <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
-        </p>
-      </div>
-</div> --}}
-
-
-
-
-
-<div class="bodybox profile-page">
-  <div class="girlsingle" itemscope="" itemtype="http://schema.org/Person">
-    <div class="profile-header">
-      <div class="profile-header-name l">
-        <h3 class="profile-title" title="Cindy and Brooks" itemprop="name">{{ $user->name  }}</h3>
-        {{-- <div class="girlsinglelabels"> <span class="orangebutton rad25">PREMIUM</span></div> --}}
-      </div>
-      <div class="profile-header-name-info rad5 r">
-        <div class="section-box"><span class="valuecolumn">167</span><b>cm</b></div>
-        <div class="section-box"><span class="valuecolumn">70</span><b>kg</b></div>
-        <div class="section-box"><span class="valuecolumn">32</span><b>years</b></div>
-      </div>
-      <div class="phone-box r">
-        <div class="label">call me</div> <a class="" href="tel:555 1234 567" itemprop="telephone"><span
-            class="icon icon-phone"></span>{{ $user->user_details->phone  }}</a>
-      </div>
-      <div class="clear10"></div>
-    </div>
-    <div class="clear10"></div>
-    <div class="thumbs" itemscope="" itemtype="http://schema.org/ImageGallery">
-      @foreach ($user->userimages as $key => $data)
-      <div class="profile-img-thumb-wrapper">
-        <div class="profile-img-thumb" id="9271" itemprop="image" itemscope="" itemtype="http://schema.org/ImageObject">
-          <a href="#"
-            data-fancybox="profile-photo" itemprop="contentURL"><img
-              data-original-url="https://escortwp.com/demo/wp-content/uploads/demo15786985966408/15786985967707-280x415.jpg"
-              class="mobile-ready-img rad3" alt="Cindy and Brooks"
-              data-responsive-img-url="https://escortwp.com/demo/wp-content/uploads/demo15786985966408/15786985967707-400x601.jpg"
-              itemprop="thumbnailUrl"
-              src="{{ asset($data->file) }}"
-            </a>
-        </div>
-      </div>
-      @endforeach
-  
-    </div>
-    <div class="clear20"></div>
-    <div class="aboutme">
-    <h4>Sobre Mi:</h4> <b> {{ strtotime($user->user_details->date_of_birth, date('ymd')) - date('ymd') }} 
-      <span itemprop="gender">{{ ucfirst($user->user_details->gender) }}</span>,
-         Soy de: <a
-          href="#" title={{ $user->user_details->province->province }}>{{ $user->user_details->province->province }}</a>, <a
-          href="#" title={{ $user->user_details->country->province }}>{{ $user->user_details->country->country }}</a></b>
-      <div class="clear5"></div> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus in quam pretium,
-      sodales velit id, aliquet dui. Vivamus nunc augue, elementum vel nulla sit amet, scelerisque aliquet sapien.
-      Vestibulum non commodo urna. Sed tempus lacus ac quam pharetra dapibus. Ut sollicitudin odio vitae nisi rhoncus
-      finibus. Nam pharetra rutrum mauris, a ultricies quam varius sed. Sed sodales felis magna, in cursus orci
-      vulputate at. Maecenas semper eros eu eros lacinia, ut pellentesque massa tincidunt.<br><br>Integer tempus justo
-      at lectus convallis, at vestibulum purus placerat. Ut ultrices enim non elit molestie fringilla ac sed sem. Fusce
-      efficitur nibh nec congue dignissim. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer dictum sed
-      magna eu posuere. Vestibulum facilisis ultricies risus quis venenatis. Fusce sed porttitor sapien. Suspendisse at
-      sapien finibus nulla rhoncus fringilla at non eros.<br><br>Vestibulum egestas interdum lectus. Donec mollis
-      sodales magna, ac facilisis ligula lacinia eu. Nunc pretium, massa ac efficitur venenatis, tellus nibh faucibus
-      metus, id pharetra sapien eros nec nisl. Maecenas sed fermentum nisl. Cras dui lectus, lobortis finibus odio
-      feugiat, scelerisque vulputate arcu. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ut ex nisi.
-      <div class="clear"></div>
-    </div>
-    <div class="clear10"></div>
-  </div>
+@section('content') 
+<div class="sidebar-right">
+  @include('user.search')
 </div>
-
+    <div class="body">
+      <div class="bodybox profile-page">
+        <div class="girlsingle" itemscope="" itemtype="http://schema.org/Person">
+          <div class="profile-header">
+            <div class="profile-header-name l">
+              <h3 class="profile-title" title="Cindy and Brooks" itemprop="name">{{ $user->name  }}</h3>
+            </div>
+            <div class="profile-header-name-info rad5 r">
+              <div class="section-box"><span class="valuecolumn">167</span><b>cm</b></div>
+              <div class="section-box"><span class="valuecolumn">70</span><b>kg</b></div>
+              <div class="section-box"><span class="valuecolumn">32</span><b>years</b></div>
+            </div>
+            <div class="phone-box r">
+              <div class="label">
+                call me
+              </div> 
+              <a class="" href={{ $user->user_details->phone  }} itemprop="telephone">
+                <span class="icon icon-phone"></span>
+                {{ $user->user_details->phone  }}
+              </a>
+            </div>
+            <div class="clear10"></div>
+          </div>
+          <div class="clear10"></div>
+          <div class="thumbs testing" style="max-height: auto; max-width: 600px;">
+            @foreach ($user->userimages as $key => $data)
+            <div class="profile-img-thumb-wrapper col-4">
+              <div class="profile-img-thumb" id="9271" itemprop="image" itemscope="" itemtype="http://schema.org/ImageObject">
+                <a href="#"
+                  data-fancybox="profile-photo" itemprop="contentURL">
+                  <img class="rounded mx-auto d-block" alt={{ $user->name }}
+                    itemprop="thumbnailUrl"
+                    src={{ asset($data->file) }} />
+              </a>
+             </div>
+            </div>
+            @endforeach
+        
+          </div>
+          <div class="clear20"></div>
+          <div class="aboutme">
+          <h4>Sobre Mi:</h4> <b> {{ strtotime($user->user_details->date_of_birth, date('ymd')) - date('ymd') }} 
+            <span itemprop="gender">{{ ucfirst($user->user_details->gender) }}</span>,
+               Soy de: <a
+                href="#" title={{ $user->user_details->province->province }}>{{ $user->user_details->province->province }}</a>, <a
+                href="#" title={{ $user->user_details->country->province }}>{{ $user->user_details->country->country }}</a></b>
+            <div class="clear5"></div> {{ $user->user_details->description  }}
+            <div class="clear"></div>
+          </div>
+          <div class="clear10"></div>
+        </div>
+      </div>
+    </div>
+  
 @endsection 
--- resources/views/user/search.blade.php
+++ resources/views/user/search.blade.php
@@ -1,23 +1,39 @@
-<div class="form-search-container">
-  <form class="buscador">
-    <div class="form-search-container-title">
-        <h4>Busqueda Rapida: </h4>
-      </div>
-     @csrf 
-     <select>
-       <option>País</option>
-       @isset($countries)
-        @foreach ($countries as $country)
-          <option value={{ $country->id }}>{{ $country->country }}</option>
-        @endforeach
-       @endisset
-     </select>
-     <select>
-       <option value="mujer">Mujer</option>
-       <option value="hombre">Hombre</option>
-       <option value="transexual">Transexual</option>
-       <option value="gay">Gay</option>
-     </select>
-      <button class="btn btn-info amd-btn" type="submit">Buscar</button>
+<div class="quicksearch">
+  <h4>Busqueda Rapida:</h4>
+  <form action="https://escortwp.com/demo/search-for-escorts/" method="post" class="form-styling"> 
+    <input type="hidden" name="action" value="search">
+    @csrf 
+    <div class="form-input col100"> <select name="country" id="country"
+        class="search-country col100 select2 select2-hidden-accessible" tabindex="-1" aria-hidden="true">
+        <option value="-1">País</option>
+        @isset($countries)
+          @foreach ($countries as $country)
+           <option class="level-0" value={{ $country->id }}>{{ $country->country }}</option>
+          @endforeach
+        @endisset
+    </select>
+        </div>
+    <div class="formseparator"></div>
+    <div class="search-cities-input form-input col100"></div>
+
+    <div class="form-input col100"> 
+      <select name="gender" class="select2 select2-hidden-accessible" tabindex="-1" aria-hidden="true">
+        <option value="1">Genero</option>
+        <option value="2">Male</option>
+        <option value="3">Couple</option>
+        <option value="4">Gay</option>
+        <option value="5">Transsexual</option>
+      </select>
+    </div>
+
+    <div class="formseparator"></div>
+ 
+    <div class="formseparator"></div>
+    <div class="center"> <input type="submit" name="submit" value="Buscar" class="submit-button blueishbutton rad3">
+      <div class="clear5"></div> <a href="https://escortwp.com/demo/search-for-escorts/" class="adv"><span
+          class="icon icon-search"></span>Busqueda Avanzada</a>
+    </div>
   </form>
+  <div class="clear"></div>
 </div>
+<div class="clear"></div>
